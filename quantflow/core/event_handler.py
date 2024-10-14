f02585f48f10edfb6eaf83e6bbf66711b2c98423
Felipe Rivera <felipe.rivera.9803@gmail.com>
2024-10-14 01:09:25 -0700

    adding event process function
    process_market_event
    process_signal_event
    process_order_request_event
    process_fill_event

--- quantflow/core/event_handler.py
+++ quantflow/core/event_handler.py
@@ -1,3 +1,6 @@
+from quantflow.core.events import SignalEvent, OrderRequestEvent
+
+
 class EventHandler:
     def __init__(self, strategy, risk_manager, portfolio, oms, event_queue):
         self.strategy = strategy
@@ -10,3 +13,19 @@ class EventHandler:
         new_event = event.process(self)
         if new_event:
             self.event_queue.put(new_event)
+
+    def process_market_event(self, event) -> SignalEvent | None:
+        signal = self.strategy.on_new_data(event.data)
+        if signal:
+            return SignalEvent(signal)
+
+    def process_signal_event(self, event) -> OrderRequestEvent | None:
+        order_request = self.risk_manager.generate_order_request(event.data)
+        if order_request:
+            return OrderRequestEvent(order_request)
+
+    def process_order_request_event(self, event) -> None:
+        self.oms.handle_order_request(event.data)
+
+    def process_fill_event(self, event) -> None:
+        self.portfolio.update_portfolio(event.data)
