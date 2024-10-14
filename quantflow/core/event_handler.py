f158fead90106624e379252d7694a8ae3e37e581
Felipe Rivera <felipe.rivera.9803@gmail.com>
2024-10-14 01:01:43 -0700

    handle events

--- quantflow/core/event_handler.py
+++ quantflow/core/event_handler.py
@@ -5,3 +5,8 @@ class EventHandler:
         self.portfolio = portfolio
         self.oms = oms
         self.event_queue = event_queue
+
+    def handle_event(self, event):
+        new_event = event.process(self)
+        if new_event:
+            self.event_queue.put(new_event)
