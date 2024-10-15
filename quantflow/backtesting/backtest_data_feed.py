ca5f5c372e2acdbca692402fa008ed24f1d5a2ee
Felipe Rivera <felipe.rivera.9803@gmail.com>
2024-10-15 14:09:55 -0700

    backtest data feed now uses pandas for easy of use
    resample is also added to help the user to choose the time frequency
    wanted

--- quantflow/backtesting/backtest_data_feed.py
+++ quantflow/backtesting/backtest_data_feed.py
@@ -1,11 +1,32 @@
 from quantflow.core.data_feed import DataFeed
 from typing import Generator
+import pandas as pd
 
 
 class BacktestDataFeed(DataFeed):
-    def __init__(self, historical_data):
-        self.historical_data = historical_data
+    def __init__(self, historical_data, datetime_col: str | None = None):
+        self.historical_data = pd.read_csv(historical_data)
+        if datetime_col:
+            self.historical_data[datetime_col] = pd.to_datetime(
+                self.historical_data[datetime_col]
+            )
+            self.historical_data.set_index(datetime_col, inplace=True)
+
+    def resample(self, period: str):
+        self.historical_data = (
+            self.historical_data.resample(period)
+            .agg(
+                {
+                    "open": "first",
+                    "high": "max",
+                    "low": "min",
+                    "close": "last",
+                    "volume": "sum",
+                }
+            )
+            .dropna()
+        )
 
     def __iter__(self) -> Generator:
-        for data in self.historical_data:
-            yield data
+        for _, row in self.historical_data.iterrows():
+            yield row.to_dict()
