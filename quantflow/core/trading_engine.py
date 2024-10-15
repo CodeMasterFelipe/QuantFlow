b13fd5a2dab52048664c68f2705556ad7c3f61f8
Felipe Rivera <felipe.rivera.9803@gmail.com>
2024-10-15 14:08:45 -0700

    make the trading engine handle the creation of the components needed
    before the user had to manually create each different component which
    made it hard for the user to easily use it specially if they don't know
    how all the parts play together

--- quantflow/core/trading_engine.py
+++ quantflow/core/trading_engine.py
@@ -1,16 +1,39 @@
-from quantflow.core.data_feed import DataFeed
-from quantflow.core.event_handler import EventHandler
-from quantflow.core.event_queue import EventQueue
+from quantflow.backtesting import BacktestDataFeed, BacktestExecutionHandler
+from quantflow.core import (
+    Strategy,
+    EventQueue,
+    EventHandler,
+    DataFeed,
+    ExecutionHandler,
+    RiskManager,
+    SimpleRiskManager,
+    Portfolio,
+    OrderManagementSystem,
+)
 from quantflow.core.events import MarketEvent
 
 
 class TradingEngine:
     def __init__(
-        self, data_feed: DataFeed, event_handler: EventHandler, event_queue: EventQueue
+        self,
+        strategy: Strategy,
+        data_feed: DataFeed | None = None,
+        execution_handler: ExecutionHandler | None = None,
+        risk_manager: RiskManager | None = None,
+        historical_data: str | None = None,
     ):
-        self.data_feed = data_feed
-        self.event_handler = event_handler
-        self.event_queue = event_queue
+        self.event_queue = EventQueue()
+        self.data_feed = data_feed or BacktestDataFeed(historical_data)
+        self.portfolio = Portfolio()
+        self.strategy = strategy
+        self.risk_manager = risk_manager or SimpleRiskManager(self.portfolio)
+        self.execution_handler = execution_handler or BacktestExecutionHandler(
+            self.event_queue, historical_data
+        )
+        self.oms = OrderManagementSystem(self.event_queue, self.execution_handler)
+        self.event_handler = EventHandler(
+            self.strategy, self.risk_manager, self.portfolio, self.oms, self.event_queue
+        )
 
     def run(self):
         data_generator = iter(self.data_feed)
