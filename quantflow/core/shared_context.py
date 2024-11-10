6618c40ea759cb66eea2420f4fdd1d3925bc5b1e
Felipe Rivera <felipe.rivera.9803@gmail.com>
2024-11-09 21:29:48 -0800

    add shared context, so there is one place for the source of truth

--- quantflow/core/shared_context.py
+++ quantflow/core/shared_context.py
@@ -1,14 +1,29 @@
+import pandas as pd
+
 class SharedContext:
     _instance = None
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
             cls._instance = super(SharedContext, cls).__new__(cls, *args, **kwargs)
-            cls._instance.latest_price = 0.0
+            cls._instance.latest_price = pd.Series()
+            cls._instance.orders = []
         return cls._instance
 
     def update_latest_price(self, price):
         self.latest_price = price
 
     def get_latest_price(self):
-        return self.latest_price
+        return self.latest_price['Close']
+    
+    def add_stoploss_takeprofit_order(self, symbol, stoploss, takeprofit, quantity):
+        self.orders.append({
+            "symbol": symbol,
+            "stoploss": stoploss,
+            "takeprofit": takeprofit,
+            "quantity": quantity
+        })
+    def get_stoploss_takeprofit_orders(self):
+        return self.orders
+    def remove_stoploss_takeprofit_order(self, order):
+        self.orders.remove(order)
