0bfcf16a56ef4be2a4497f2a84e4fe45ba7a7171
Felipe Rivera <felipe.rivera.9803@gmail.com>
2024-11-10 11:05:44 -0800

    add logic to keep track of good trades and bad trades and get the trade accuracy

--- quantflow/core/portfolio.py
+++ quantflow/core/portfolio.py
@@ -1,6 +1,7 @@
 from quantflow.core.events import FillEvent
-from quantflow.core.types import SignalType
+from quantflow.core.types import Fill, OrderSide, SignalType
 from quantflow.core.shared_context import SharedContext
+import logging
 
 
 class Portfolio:
@@ -8,6 +9,8 @@ class Portfolio:
         self.positions = {}
         self.cash = 0.0
         self.shared_context = SharedContext()
+        self.good_trades = 0
+        self.bad_trades = 0
 
     def update_portfolio(self, fill: Fill):
         symbol = fill.symbol
@@ -20,6 +23,15 @@ class Portfolio:
         elif fill.order_side == OrderSide.SELL:
             self.cash += fill.fill_price * fill.fill_quantity
             self.positions[symbol] -= fill.fill_quantity
+        
+        if "takeprofit" in fill.order_id:
+            self.good_trades += 1
+            logging.info(f"Good trade: {self}")
+        elif "stoploss" in fill.order_id:
+            self.bad_trades += 1
+            logging.info(f"Bad trade: {self}")
+        
+        logging.info(f"Portfolio updated: {self}")
 
     def get_position(self, symbol):
         return self.positions.get(symbol, 0)
@@ -30,6 +42,9 @@ class Portfolio:
         for _, position in self.positions.items():
             total_equity += position * latest_price
         return total_equity
+    
+    def get_accuracy(self):
+        return self.good_trades / (self.good_trades + self.bad_trades)
 
     def __str__(self):
         return f"Portfolio(cash={self.cash}, positions={self.positions})"
