2f2fe4d3dde1f1fd2a5521b6b5178b22affaa3c8
Felipe Rivera <felipe.rivera.9803@gmail.com>
2024-11-09 21:38:36 -0800

    Update of the data type, and implementation of the check for stop loss and take profit

--- quantflow/backtesting/backtest_execution_handler.py
+++ quantflow/backtesting/backtest_execution_handler.py
@@ -1,6 +1,6 @@
 from quantflow.core.execution_handler import ExecutionHandler
 from quantflow.core.events import FillEvent
-from quantflow.core.types import Fill
+from quantflow.core.types import Fill, OrderSide
 from quantflow.core.shared_context import SharedContext
 
 
@@ -16,7 +16,7 @@ class BacktestExecutionHandler(ExecutionHandler):
         self.event_queue = event_queue
         self.shared_context = SharedContext()
 
-    def execute_order(self, order):
+    def execute_order(self, order) -> Fill:
         """
         Simulates the execution of an order in a backtest environment
         Assumes order are filled immediately at the current price from the market data
@@ -28,9 +28,41 @@ class BacktestExecutionHandler(ExecutionHandler):
         fill = Fill(
             order_id=order.order_id,
             symbol=order.symbol,
-            fill_price=current_price,
+            fill_price=order.price,
             fill_quantity=order.quantity,
             order_side=order.side,
         )
-        fill_event = FillEvent(fill)
-        self.event_queue.put(fill_event)
+        self.shared_context.add_stoploss_takeprofit_order(order.symbol, stoploss=order.stop_loss, takeprofit=order.take_profit, quantity=order.quantity)
+
+        return fill
+    
+    def check_stoploss_takeprofit(self):
+        """check if stoploss or takeprofit is hit if so, send back a fill signal which should update the portfolio later.
+        """
+        current_price = self.shared_context.latest_price
+
+        
+        for order in self.shared_context.get_stoploss_takeprofit_orders():
+            if current_price['Low'] <= order["stoploss"]:
+                fill = Fill(
+                    order_id="stoploss",
+                    symbol=order["symbol"],
+                    # fill_price=current_price,
+                    fill_price=order['stoploss'],
+                    # fill_price=current_price['Close'],
+                    fill_quantity=order["quantity"],
+                    order_side=OrderSide.SELL
+                )
+                self.event_queue.put(FillEvent(fill))
+                self.shared_context.remove_stoploss_takeprofit_order(order)
+            elif current_price['High'] >= order["takeprofit"]:
+                fill = Fill(
+                    order_id="takeprofit",
+                    symbol=order["symbol"],
+                    # fill_price=current_price,
+                    fill_price=order['takeprofit'],
+                    fill_quantity=order["quantity"],
+                    order_side=OrderSide.SELL
+                )
+                self.event_queue.put(FillEvent(fill))
+                self.shared_context.remove_stoploss_takeprofit_order(order)
