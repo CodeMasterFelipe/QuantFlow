952f029da203540d608d2cb427e7a5b1c006839a
Felipe Rivera <felipe.rivera.9803@gmail.com>
2024-11-09 21:35:05 -0800

    add type hinting to the execute_order

--- quantflow/core/execution_handler.py
+++ quantflow/core/execution_handler.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
+from quantflow.core.types import Fill
 
 
 class ExecutionHandler(ABC):
     @abstractmethod
-    def execute_order(self, order):
+    def execute_order(self, order) -> Fill:
         """
         Execute and order. This method should be implemented by concrete subclasses for backtesting or live trading.
         """
